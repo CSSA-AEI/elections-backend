8df2f99056196574ea13a0795290349f0c654082
aMahanna <anthony.mahanna@gmail.com>
2021-10-27 21:04:00 -0400

    clean up populate.ts logic

--- src/scripts/populate.ts
+++ src/scripts/populate.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import '../database';
+import { schema, delay, getHTMLMessage } from './utils';
 import mailgun from 'mailgun-js';
 import crypto from 'crypto';
 import { UserObject } from '../model/User';
@@ -13,81 +14,13 @@ const readXlsxFile = require('read-excel-file/node');
 const ADMIN_EMAIL: string = process.env.ADMIN_EMAIL!;
 const MAILGUN_API_KEY: string = process.env.MAILGUN_API_KEY!;
 const MAILGUN_DOMAIN: string = process.env.MAILGUN_DOMAIN!;
-const VOTING_START_EST = new Date(process.env.VOTING_START!);
+
+const VOTING_START_EST: any = new Date(process.env.VOTING_START!);
+const VOTING_END_EST: any = new Date(process.env.VOTING_DEADLINE!);
 const VOTING_START_UTC: string = new Date(VOTING_START_EST.getTime() + new Date().getTimezoneOffset() * 60 * 1000).toUTCString();
+const VOTING_DURATION_HOURS = (Math.abs(VOTING_END_EST - VOTING_START_EST) / 36e5).toString();
 
 const mg = mailgun({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN });
-
-const schema = {
-  id: {
-    prop: 'id',
-    type: String,
-  },
-  SESSION: {
-    prop: 'session',
-    type: String,
-  },
-  'PRÉNOM OFFICIEL': {
-    prop: 'prenomofficiel',
-    type: String,
-  },
-  'NOM OFFICIEL': {
-    prop: 'nomofficiel',
-    type: String,
-  },
-  'PRÉNOM PRÉFÉRÉ': {
-    prop: 'prenomprefere',
-    type: String,
-  },
-  'NOM PRÉFÉRÉ': {
-    prop: 'nomprefere',
-    type: String,
-  },
-  COURRIEL: {
-    prop: 'courriel',
-    type: String,
-  },
-  'LANGUE DE COMMUNICATION': {
-    prop: 'langue',
-    type: String,
-  },
-  'CHARGE ACADÉMIQUE': {
-    prop: 'langue',
-    type: String,
-  },
-  GROUP: {
-    prop: 'group',
-    type: String,
-  },
-  'PROG DESCR': {
-    prop: 'program',
-    type: String,
-  },
-  DISCIPLINE: {
-    prop: 'discipline',
-    type: String,
-  },
-  'PLAN DESCR': {
-    prop: 'plan',
-    type: String,
-  },
-  LEVEL: {
-    prop: 'level',
-    type: String,
-  },
-  'RSG 1': {
-    prop: 'rsg1',
-    type: String,
-  },
-  'RSG 2': {
-    prop: 'rsg2',
-    type: String,
-  },
-};
-
-function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 function generateSha(name: string, email: string, id: string): string {
   const length = 16;
@@ -123,8 +56,8 @@ async function generateVoters(path: string): Promise<void> {
           'o:deliverytime': VOTING_START_UTC,
           from: ADMIN_EMAIL,
           to: email,
-          subject: 'VOTE(Z): CSSA-AÉI 2022-2023',
-          html: `<!DOCTYPE html><html><body>The CSSA By-Elections open October 29 at 9AM!<br></br>If you would like to participate in the election (starting 9AM on October 29), your unique voting link is:<br></br> <a href=${link}>${link}</a><br></br> The deadline to vote is October 31st at 9AM<br>Questions or issues? Your point of contact is it@cssa-aei.ca<br></br>Thank you for voting!<br> - Your CSSA exec team<br></br><br></br><br></br>Les élections partielles de l'AÉI débutent le 29 Octobre à 9h!<br></br> Si vous souhaitez participer à cette élection (qui commence le 29 Octobre à 9h), votre lien de vote est:<br></br> <a href=${link}>${link}</a><br></br> La date limite pour voter est le 31 Octobre à 9h<br>Questions ou problèmes? Votre point de contact est it@cssa-aei.ca<br></br> Merci pour votre participation!<br> - Votre équipe de direction de l'AÉI </body></html>`,
+          subject: 'VOTE(Z): CSSA-AÉI',
+          html: getHTMLMessage(link, VOTING_DURATION_HOURS),
         });
         console.log(message);
 
@@ -145,7 +78,7 @@ async function generateVoters(path: string): Promise<void> {
   console.log(`URI: ${process.env.MONGO_URI}`);
   console.log(`Starting in ${delayTime / 1000} seconds...`);
   for (let i = delayTime / 1000 - 1; i >= 1; i--) {
-    console.log(i)
+    console.log(i);
     await delay(1000);
   }
 
