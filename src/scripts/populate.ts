c8b4bc4b9f56a5c2b9d0e5a95d5b5887da6c4736
aMahanna <anthony.mahanna@gmail.com>
2021-10-27 21:50:10 -0400

    new: prompt-sync dev dependency

--- src/scripts/populate.ts
+++ src/scripts/populate.ts
@@ -1,22 +1,24 @@
 /* eslint-disable */
-import '../database';
-import { schema, delay, getHTMLMessage } from './utils';
 import mailgun from 'mailgun-js';
 import crypto from 'crypto';
+
+import '../database';
+import { schema, delay, getHTMLMessage } from './utils';
 import { UserObject } from '../model/User';
+
+const prompt = require('prompt-sync')({ sigint: true });
+const readXlsxFile = require('read-excel-file/node');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const readXlsxFile = require('read-excel-file/node');
-
 const ADMIN_EMAIL: string = process.env.ADMIN_EMAIL!;
 const MAILGUN_API_KEY: string = process.env.MAILGUN_API_KEY!;
 const MAILGUN_DOMAIN: string = process.env.MAILGUN_DOMAIN!;
-
 const VOTING_START_EST: any = new Date(process.env.VOTING_START!);
 const VOTING_END_EST: any = new Date(process.env.VOTING_DEADLINE!);
+
 const VOTING_START_UTC: string = new Date(VOTING_START_EST.getTime() + new Date().getTimezoneOffset() * 60 * 1000).toUTCString();
 const VOTING_DURATION_HOURS = (Math.abs(VOTING_END_EST - VOTING_START_EST) / 36e5).toString();
 
@@ -71,12 +73,27 @@ async function generateVoters(path: string): Promise<void> {
 }
 
 (async () => {
-  const delayTime = 15000;
   const PATH = '/../../../assets/test.xlsx';
 
+  console.log('ATTENTION: You are about to run a risk-prone script. Review:');
+  console.log('\n\n----------------------------------');
   console.log(`PATH: ${PATH}`);
-  console.log(`URI: ${process.env.MONGO_URI}`);
-  console.log(`Starting in ${delayTime / 1000} seconds...`);
+  console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
+  console.log(`VOTING_START_EST: ${VOTING_START_EST}`);
+  console.log(`VOTING_START_UTC: ${VOTING_START_UTC}`);
+  console.log(`VOTING_DURATION_HOURS: ${VOTING_DURATION_HOURS}`);
+  console.log(`ADMIN_EMAIL: ${ADMIN_EMAIL}`);
+  console.log(`MAILGUN_DOMAIN: ${MAILGUN_DOMAIN}`);
+  console.log(`MAILGUN_API_KEY: ${MAILGUN_API_KEY}`);
+  console.log('----------------------------------\n\n');
+
+  const input = prompt('ARE YOU CERTAIN YOU WISH TO PROCEED? (yes/no): ');
+  if (input !== 'yes') {
+    throw Error('User is a chicken');
+  }
+
+  const delayTime = 10000;
+  console.log(`Starting in ${delayTime / 1000} seconds. glhf`);
   for (let i = delayTime / 1000 - 1; i >= 1; i--) {
     console.log(i);
     await delay(1000);
