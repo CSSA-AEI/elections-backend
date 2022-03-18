687232b25c2f3a9ca1264b3e23a7490eb5710b03
aMahanna <anthony.mahanna@gmail.com>
2022-03-18 18:23:45 -0400

    new: demo candidates

--- assets/candidates.ts
+++ assets/candidates.ts
@@ -25,6 +25,22 @@
  *  { name: 'Sami Ben-Moussa', val: 'benmoussa' }
  * ]
  */
+
+/** @note - Mario candidates are for demo purposes only */
+// const CANDIDATES = {
+//   PRES: [{ name: 'Mario', val: 'mario' }, { name: 'Waluigi', val: 'waluigi' }],
+//   FNCE: [{ name: 'Yoshi', val: 'yoshi' }, { name: 'Bowser', val: 'bowser' }],
+//   ACDM: [{ name: 'Toad', val: 'toad' }, { name: 'Luigi', val: 'luigi' }],
+//   COMS: [{ name: 'Wario', val: 'wario' }, { name: 'Princess Daisy', val: 'daisy' }],
+//   INTR: [{ name: 'Rosalina', val: 'rosalina' }, { name: 'Diddy Kong', val: 'diddy' }],
+//   EXTR: [{ name: 'Princess Peach', val: 'peach' }, { name: 'Birdo', val: 'birdo' }],
+//   SOCL: [{ name: 'Donkey Kong', val: 'donkey' }, { name: 'Kamek', val: 'kamek' }],
+//   PHIL: [{ name: 'King Boo', val: 'boo' }],
+//   EQUT: [{ name: 'Bowser JR', val: 'bowserjr' }],
+//   EXAF: [{ name: 'Baby Mario', val: 'babymario' }],
+//   INTE: [{ name: 'Goomba', val: 'goomba' }],
+// };
+
 const CANDIDATES = {
   PRES: [{ name: 'FirstName LastName', val: 'lastname' }],
   FNCE: [{ name: 'FirstName LastName', val: 'lastname' }],
